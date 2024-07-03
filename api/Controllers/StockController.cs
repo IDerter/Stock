50394e9ef51070377ce0afa1d153026ff57976a3
IDerter <101259146+IDerter@users.noreply.github.com>
2024-07-03 12:46:31 +0300

    Initial commit

---
+++ api/Controllers/StockController.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using api.Dtos.Stock;
+using api.Mappers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/stock")] 
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+
+        public StockController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var stocks = _context.Stocks.Select(stock => stock.ToStockDto()).ToList(); 
+
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var stock = _context.Stocks.Find(id);
+
+            if (stock == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(stock.ToStockDto());
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateStockRequestDto stockRequestDto)
+        {
+            var stockModel = stockRequestDto.ToStockFromCreateDto();
+            _context.Stocks.Add(stockModel);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetById), new {id = stockModel.Id}, stockModel.ToStockDto());
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateStock)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+        /*
+            stockModel.Symbol = updateStock.Symbol;
+            stockModel.CompanyName = updateStock.CompanyName;
+            stockModel.Purchase = updateStock.Purchase;
+            stockModel.LastDiv = updateStock.LastDiv;
+            stockModel.Industry = updateStock.Industry;
+            stockModel.MarketCap = updateStock.MarketCap;
+        */
+            
+            _context.Entry(updateStock).CurrentValues.SetValues(stockModel);
+            _context.SaveChanges();
+
+            return Ok(stockModel.ToStockDto());
+        }
+    }
+}
---
+++ api/Mappers/StockMappers.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Dtos.Stock;
+using api.Models;
+
+namespace api.Mappers
+{
+    public static class StockMappers
+    {
+        public static StockDto ToStockDto (this Stock stockModel)
+        {
+            return new StockDto
+            {
+                Id = stockModel.Id,
+                Symbol = stockModel.Symbol,
+                CompanyName = stockModel.CompanyName,
+                Purchase = stockModel.Purchase,
+                LastDiv = stockModel.LastDiv,
+                Industry = stockModel.Industry,
+                MarketCap = stockModel.MarketCap
+            };
+        }
+
+        public static Stock ToStockFromCreateDto(this CreateStockRequestDto stockRequestDto)
+        {
+            return new Stock
+            {
+                Symbol = stockRequestDto.Symbol,
+                CompanyName = stockRequestDto.CompanyName,
+                Purchase = stockRequestDto.Purchase,
+                LastDiv = stockRequestDto.LastDiv,
+                Industry = stockRequestDto.Industry,
+                MarketCap = stockRequestDto.MarketCap
+            };
+        }
+    }
+}
