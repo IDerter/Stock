7e4bddc1cb1e5faec978104db4e7483c5ab275a5
IDerter <101259146+IDerter@users.noreply.github.com>
2024-07-03 17:33:19 +0300

    StockRepository
    Add interface IStockRepository, implements all in class StockRepository and refactor code with new methods from StockRepository in StockController, inject in constructor

---
+++ api/Repository/StockRepository.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using api.Dtos.Stock;
+using api.Interfaces;
+using api.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Repository
+{
+    public class StockRepository : IStockRepository
+    {
+        private readonly ApplicationDBContext _context;
+
+        public StockRepository(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Stock> CreateAsync(Stock stockModel)
+        {
+            await _context.Stocks.AddAsync(stockModel);
+            await _context.SaveChangesAsync();
+            return stockModel;
+        }
+
+        public async Task<Stock?> DeleteAsync(int id)
+        {
+            var stock =  await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+            if (stock == null)
+                return null;
+            
+            _context.Stocks.Remove(stock);
+            await _context.SaveChangesAsync();
+            return stock;
+        }
+
+        public async Task<List<Stock>> GetAllAsync()
+        {
+            return await _context.Stocks.ToListAsync();
+        }
+
+        public async Task<Stock?> GetByIdAsync(int id)
+        {
+            return await _context.Stocks.FindAsync(id);
+        }
+
+        public async Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto)
+        {
+            var stock = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+            if (stock == null)
+                return null;
+
+            stock.Industry = stockDto.Industry;
+            stock.CompanyName = stockDto.CompanyName;
+            stock.LastDiv = stockDto.LastDiv;
+            stock.MarketCap = stockDto.MarketCap;
+            stock.Purchase = stockDto.Purchase;
+            stock.Symbol = stockDto.Symbol;
+
+            return stock;
+        }
+    }
+}
