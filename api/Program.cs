2ec029e580369dac11a29a453aa161c1dc468abc
IDerter <101259146+IDerter@users.noreply.github.com>
2024-07-04 17:24:23 +0300

    Comment System
    Add multiple comment in 1 stock, test in swagger, add comments in local db

--- api/Program.cs
+++ api/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddDbContext<ApplicationDBContext>(options =>  {
 });
 
 builder.Services.AddScoped<IStockRepository, StockRepository>();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
 
 var app = builder.Build();
 
